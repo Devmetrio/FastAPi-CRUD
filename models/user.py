2def6f4fd5ca53d37bd065bc34e27e4b5b9cd737
German <briph21@gmail.com>
2024-10-08 01:52:06 -0500

    Solucion de responses

--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Integer, String, Table, Column
 from config.db import meta, engine
 users = Table("users", meta, 
-              Column("id", Integer, primary_key=True), 
+              Column("id", Integer, primary_key=True, autoincrement=True), 
               Column("name", String(40)),
               Column("email", String(40)),
-              Column("password", String(40)))
+              Column("password", String(255)))
 
 
 meta.create_all(engine)
--- routes/user.py
+++ routes/user.py
@@ -15,10 +15,23 @@ def get_Users():
 
 @user.post('/user')
 def create_user(user: User):
-    new_user = {"name": user.name, "email": user.email}
-    new_user["password"] = f.encrypt(user.password.encode("utf-8")) 
-    result = conn.execute(users.insert().values(new_user))
-    return conn.execute(users.select().where(users.c.id == result.lastrowid)).first()
+    try:
+        new_user = {"name": user.name, "email": user.email}
+        new_user["password"] = f.encrypt(user.password.encode("utf-8"))
+        result = conn.execute(users.insert().values(new_user))
+        conn.commit()
+
+        # Obtener el ID del usuario insertado
+        last_inserted_id = result.lastrowid
+        # Consultar el usuario recién insertado y convertirlo a un diccionario
+        inserted_user = conn.execute(users.select().where(users.c.id == last_inserted_id)).first()
+        # Convertir el resultado a un diccionario antes de devolverlo
+        inserted_user_dict = dict(inserted_user._asdict())
+        return inserted_user_dict
+    
+    except SQLAlchemyError as e:
+        print(str(e))
+        return {"error": str(e)}
 
 @user.get('/user/{id}')
 def get_user(id: str):
@@ -26,11 +39,15 @@ def get_user(id: str):
 
 
 @user.delete('/user/{id}')
-def delete_user():
+def delete_user(id: str):
     conn.execute(users.delete().where(users.c.id == id))
     return Response(status_code = HTTP_204_NO_CONTENT)
     
 @user.put('/user/{id}')
 def update_user(id:str , user: User):
-    conn.execute(users.update().value(name = user.name, email = user.email, password = f.encrypt(user.password).encode("utf-8"))).where(users.c.id == id)
+    conn.execute(users.update().where(users.c.id == id).values(
+        name=user.name,
+        email=user.email,
+        password=f.encrypt(user.password.encode("utf-8"))
+    ))
     return conn.execute(users.select().where(users.c.id == id)).first()
